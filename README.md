0bac885886514b6ff86d9779f5d70ea15c91e982
Hunter Lindauer <holindauer@gmail.com>
2024-01-09 12:55:25 -0800

    ran successful training

--- README.md
+++ README.md
@@ -1,14 +1,35 @@
 # Siamese-Network Image Classification
 
-## What is a Siamese Neural Network?
+This repository implements a Siamese Neural Network for the task of multi-class image classificaiton on the CIFDAR-100 dataset.
 
-Siamse-Net is a neural network architecture that is designed to learn a mapping from
-some input space to a smaller vector space such that similar inputs lead to vector 
-embeddings in a similar location in the vector space. This means the network can be 
-used to measure the similarity between two inputs, which is useful for tasks such as 
-classification, search, and verification.
+## Siamese Net Architecture
 
-### Project Overview:
+The siamese net is a neural network architecture that learns a mapping from some input space to a smaller embedding space. The network accepts two (potentially more) labeled inputs, passing each of them 
+through the same network. That is, two separate forward passes are performed using the same weights for each input. The network outputs two vector embeddings, which can be compared using a distance metric for tasks such as classification, search, and verification. 
+
+The benefit of this approach is that the network can precompute embeddings and store them in a database, which can be queried at a later time. For the application of search, storing precomputed embeddings for similarity comparison is vastly more efficient than, say, running inference on a binary classifier optimized to determine if a query matches an input for the entire database.
+
+## Training 
+
+The siamese network is optimized to minimize a distance metric for inputs of the same class, and maximize that distance for inputs of different classes. In this implementation I chose the 
+contrastive loss function, which is defined as follows:
+
+$$
+J(\theta) = \frac{1}{2} \left[ y \cdot D^2 + (1 - y) \cdot \max(0, m - D)^2 \right]
+$$
+
+Where:
+- $D$ is the distance metric between embeddings 
+- $y$ is a binary label regarding if the inputs belong to the same class
+- $m$ is the margin, a hyperparameter that controls the minimum allowed distance between embeddings for inputs of different classes.
+
+This loss function can be understood as the average of the two inner terms. When $y$ is 1, the first term is used, and the network is rewarded for minimizing the distance between embeddings. When $y$ is 0, the second term is used, and the network is penalized for embeddings that are closer than the margin $m$.
+
+I conducted training in an AWS ml.g4dn.xlarge sagemaker notebook instance, all model/training components being imported into this [notebook](training_env.ipynb). The training script is located in [train.py](train.py). The model was written in pytorch, and can be found in [siamese_net.py](siamese_net.py). As well, I am using a custom [dataloader](dataloader.py) to ensure equeal class distribution in each batch. The distribution of like and unlike pairs that is, not the distribution of classes. As well as [early stopping regularization](early_stopping.py).
+
+
+
+### Project History:
 Originally, this project was intended for verification of handwritten signatures. However, I was
 not able to source enough high quality data to train a model that I was truly satisfied
 with. After coming back to the project after a few months, I'm going to re-purpose the 
--- train.py
+++ train.py
@@ -2,10 +2,8 @@ import torch
 from torch import Tensor
 from torch.utils.data import DataLoader
 import torch.nn as nn
-import copy
 from dataclasses import dataclass
 from early_stopping import EarlyStopping
-from typing import Tuple
 
 # training config
 @dataclass
@@ -19,118 +17,105 @@ class TrainConfig:
     optimizer: torch.optim
     patience: int
     es_min_delta: float
-    
 
 class Trainer:
     def __init__(self, config: TrainConfig):   
-
-        # unpack config
+        # Unpack config
         self.model = config.model
-
         self.trainloader = config.trainloader
         assert self.trainloader.batch_size % 2 == 0, "Batch size must be even"
-
         self.testloader = config.testloader
         assert self.testloader.batch_size % 2 == 0, "Batch size must be even"
-
         self.epochs = config.epochs
         self.device = config.device
         self.optimizer = config.optimizer
         self.patience = config.patience
 
-        # define loss function
-        self.contrastive_loss = nn.CosineEmbeddingLoss(margin=0.5, reduction='mean')
+        # Define loss function
+        self.contrastive_loss = nn.CosineEmbeddingLoss(margin=config.contrastive_margin, reduction='mean')
 
-        # training/test loss
+        # Training/test loss and accuracy
         self.train_loss = [0 for i in range(self.epochs)]
         self.test_loss = [0 for i in range(self.epochs)]
+        self.train_accuracy = [0 for i in range(self.epochs)]
+        self.test_accuracy = [0 for i in range(self.epochs)]
 
-        # setup early stopping
+        # Setup early stopping
         self.early_stopping = EarlyStopping(patience=self.patience, min_delta=0.0)
 
     def train(self):
-    
-        #send model to device
+        
         self.model.to(self.device)
         print(f"Model sent to {self.device}")
 
         for epoch in range(self.epochs):
             
-            # set model to train mode
             self.model.train() 
+            correct_train_predictions = 0
+            total_train_predictions = 0
 
-     
             for (imgs1, imgs2, labels) in self.trainloader:
+                imgs1, imgs2 = imgs1.to(self.device), imgs2.to(self.device)
+                labels = labels.to(self.device)
 
-                #send data to device
-                imgs1, imgs2 = imgs1.to(self.device), imgs1.to(self.device)
-
-                # zero the parameter gradients
+                # Zero the parameter gradients
                 self.optimizer.zero_grad()
 
-                # forward 
-                embedding1, embedding1 = self.model(imgs1, imgs2)
+                # Forward 
+                embedding1, embedding2 = self.model(imgs1, imgs2)
+                labels = labels.squeeze()
+                loss = self.contrastive_loss(embedding1, embedding2, labels)
 
-                # Compute the loss
-                loss = self.contrastive_loss(embedding1, embedding1, labels)
-
-                # Zero the gradients before running the backward pass
-                self.optimizer.zero_grad()
-
-                # Backward pass
+                # Backward pass and update weights
                 loss.backward()
-
-                # Update weights
                 self.optimizer.step()
 
-                # update training loss
+                # Update training loss
                 self.train_loss[epoch] += loss.item()
 
-            #average training loss
-            self.train_loss[epoch] /= len(self.trainloader)
+                # Calculate predictions and update accuracy
+                cos_sim = torch.nn.functional.cosine_similarity(embedding1, embedding2)
+                predicted_labels = (cos_sim > 0.5).float()
+                correct_train_predictions += (predicted_labels == labels).sum().item()
+                total_train_predictions += labels.size(0)
 
-            # evaluate model on test set
+            # Average training loss and accuracy
+            self.train_loss[epoch] /= len(self.trainloader)
+            self.train_accuracy[epoch] = correct_train_predictions / total_train_predictions
+
+            # Evaluate model on test set
             self.eval(epoch)
 
-            # check for early stopping
+            # Check for early stopping
             if self.early_stopping(self.test_loss[epoch], self.model):
                 break
 
-            print(f"Epoch {epoch+1} training loss: {self.train_loss[epoch]} test loss: {self.test_loss[epoch]}")
+            print(f"Epoch {epoch+1}: Training Loss: {self.train_loss[epoch]}, Training Accuracy: {self.train_accuracy[epoch]}, Test Loss: {self.test_loss[epoch]}, Test Accuracy: {self.test_accuracy[epoch]}")
 
-    def eval(self, epoch : int):
-
-        # set model to eval mode
+    def eval(self, epoch: int):
         self.model.eval()
+        correct_test_predictions = 0
+        total_test_predictions = 0
 
         with torch.no_grad():
             for (imgs1, imgs2, labels) in self.testloader:
-                    
-                # send data to device
                 imgs1, imgs2 = imgs1.to(self.device), imgs2.to(self.device)
+                labels = labels.to(self.device)
 
-                # forward 
-                embedding1, embedding1 = self.model(imgs1, imgs2)
+                # Forward
+                embedding1, embedding2 = self.model(imgs1, imgs2)
+                labels = labels.squeeze()
+                loss = self.contrastive_loss(embedding1, embedding2, labels)
 
-                # Compute the loss
-                loss = self.contrastive_loss(embedding1, embedding1, labels)
-
-                # Zero the gradients before running the backward pass
-                self.optimizer.zero_grad()
-
-                # Backward pass
-                loss.backward()
-
-                # Update weights
-                self.optimizer.step()
-
-                # update test loss
+                # Update test loss
                 self.test_loss[epoch] += loss.item()
 
-            #average test loss
+                # Calculate predictions and update accuracy
+                cos_sim = torch.nn.functional.cosine_similarity(embedding1, embedding2)
+                predicted_labels = (cos_sim > 0.5).float()
+                correct_test_predictions += (predicted_labels == labels).sum().item()
+                total_test_predictions += labels.size(0)
+
+            # Average test loss and accuracy
             self.test_loss[epoch] /= len(self.testloader)
-
-            
-
-
-                
+            self.test_accuracy[epoch] = correct_test_predictions / total_test_predictions
